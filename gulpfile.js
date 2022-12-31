8f7148ae441936b9d939b4c66c1af4309418e88d
William <williamfs133@gmail.com>
2022-12-31 12:17:49 -0300

    hero section: done

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
 
 function buildStyles() {
     return gulp.src('./src/styles/*.scss')
@@ -7,7 +8,18 @@ function buildStyles() {
         .pipe(gulp.dest('./dist/styles'));
 }
 
-exports.default = gulp.series(buildStyles);
+function imageMin() {
+    return gulp.src('./src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'));
+}
+
+function distVideo() {
+    return gulp.src('./src/video/*')
+    .pipe(gulp.dest('./dist/video'));
+}
+
+exports.default = gulp.parallel(buildStyles, imageMin, distVideo);
 exports.watch = function() {
     gulp.watch('./src/**/styles/*.scss', gulp.series(buildStyles));
 }
